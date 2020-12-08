432fa8e24495a6d7faaee984acb012eb0b220fb3
Reyhn3 <bjorn@sentiandream.com>
2020-12-08 15:40:25 +0100

    Implement the IEventPersistence for Azure Storage tables

---
+++ Source/EventFlow.AzureStorage.IntegrationTests/EventStores/BlobOptimisticSyncStoreTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using EventFlow.AzureStorage.Config;
+using EventFlow.AzureStorage.Connection;
+using EventFlow.AzureStorage.EventStores;
+using EventFlow.TestHelpers;
+using NUnit.Framework;
+using Shouldly;
+
+
+namespace EventFlow.AzureStorage.IntegrationTests.EventStores
+{
+	[Explicit("Integration tests")]
+	[Category(Categories.Integration)]
+	public class BlobOptimisticSyncStoreTests
+	{
+		private BlobOptimisticSyncStore _target;
+
+		[SetUp]
+		public async Task PreRun()
+		{
+			var config = new AzureStorageConfiguration
+				{
+					StorageAccountConnectionString = "UseDevelopmentStorage=true",
+					SystemContainerName = "eventflow-system-params",
+					EventStoreTableName = "EventStoreEvents"
+				};
+			var factory = new AzureStorageFactory(config);
+			await factory.InitializeAsync().ConfigureAwait(false);
+			_target = new BlobOptimisticSyncStore(factory);
+			await _target.InitializeAsync().ConfigureAwait(false);
+		}
+
+		[Test]
+		public async Task InitializeAsync_should_create_container_and_blob_with_value_zero_if_it_does_not_exist()
+		{
+			await _target.InitializeAsync();
+			var current = await _target.GetCurrentAsync();
+			Console.WriteLine(current);
+			current.ShouldBe(0);
+		}
+
+		[Test(Description = "Read the current value")]
+		public async Task GetDataAsync_should_retrieve_the_current_value()
+		{
+			var result = await _target.GetCurrentAsync();
+			Console.WriteLine(result);
+		}
+
+		[Test(Description = "Set a specific value")]
+		public async Task TryOptimisticWriteAsync_should_write_new_value()
+		{
+			var result = await _target.TryOptimisticWriteAsync(0);
+			result.ShouldBeTrue();
+		}
+	}
+}
--- Source/EventFlow.AzureStorage/EventStores/AzureStoragePersistence.cs
+++ Source/EventFlow.AzureStorage/EventStores/AzureStoragePersistence.cs
@@ -1,25 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using EventFlow.Aggregates;
 using EventFlow.AzureStorage.Connection;
 using EventFlow.Core;
 using EventFlow.EventStores;
 using EventFlow.Logs;
 using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Azure.Cosmos.Table.Protocol;
 
 
 namespace EventFlow.AzureStorage.EventStores
 {
 	public class AzureStoragePersistence : IEventPersistence
 	{
+		/// <summary>
+		///     The row key is an int, and its <see cref="int.MaxValue" /> is
+		///     ten digits long. To enable efficient sorting and querying by
+		///     range, pad with leading zeros.
+		/// </summary>
+		private const string RowKeyFormatString = "D10";
+
 		private readonly ILog _log;
 		private readonly IAzureStorageFactory _factory;
+		private readonly IUniqueIdGenerator _uniqueIdGenerator;
 
-		public AzureStoragePersistence(ILog log, IAzureStorageFactory factory)
+		public AzureStoragePersistence(ILog log, IAzureStorageFactory factory, IUniqueIdGenerator uniqueIdGenerator)
 		{
 			_log = log ?? throw new ArgumentNullException(nameof(log));
 			_factory = factory ?? throw new ArgumentNullException(nameof(factory));
+			_uniqueIdGenerator = uniqueIdGenerator ?? throw new ArgumentNullException(nameof(uniqueIdGenerator));
 		}
 
 		public async Task<AllCommittedEventsPage> LoadAllCommittedEvents(
@@ -27,7 +39,41 @@ namespace EventFlow.AzureStorage.EventStores
 			int pageSize, 
 			CancellationToken cancellationToken)
 		{
-			throw new NotImplementedException();
+			var table = _factory.CreateTableReferenceForEventStore();
+			var results = new List<EventDataEntity>();
+			TableQuery<EventDataEntity> query;
+
+			if (globalPosition.IsStart)
+			{
+				query = new TableQuery<EventDataEntity>().Take(pageSize);
+			}
+			else
+			{
+				var startPosition = long.Parse(globalPosition.Value);
+				var filter = TableQuery.GenerateFilterConditionForLong(nameof(EventDataEntity.GlobalSequenceNumber), QueryComparisons.GreaterThanOrEqual, startPosition);
+				query = new TableQuery<EventDataEntity>().Where(filter).Take(pageSize);
+			}
+
+			TableContinuationToken token = null;
+			do
+			{
+				var resultSegment = await table.ExecuteQuerySegmentedAsync(query, token, cancellationToken).ConfigureAwait(false);
+				token = resultSegment.ContinuationToken;
+
+				results.AddRange(resultSegment.Results);
+			} while (token != null);
+
+			var nextPosition = results.Any()
+				? results.Max(e => e.GlobalSequenceNumber) + 1
+				: 0;
+
+			var events = results
+				.OrderBy(m => m.GlobalSequenceNumber)
+				.Select(m => m.ToDomainEvent())
+				.ToList()
+				.AsReadOnly();
+
+			return new AllCommittedEventsPage(new GlobalPosition(nextPosition.ToString()), events);
 		}
 
 		public async Task<IReadOnlyCollection<ICommittedDomainEvent>> CommitEventsAsync(
@@ -35,7 +81,34 @@ namespace EventFlow.AzureStorage.EventStores
 			IReadOnlyCollection<SerializedEvent> serializedEvents,
 			CancellationToken cancellationToken)
 		{
-			throw new NotImplementedException();
+			if (!serializedEvents.Any())
+				return Array.Empty<ICommittedDomainEvent>();
+
+			var entityTask = await Task.WhenAll(serializedEvents
+					.Select(async e => new EventDataEntity(id.Value, e.AggregateSequenceNumber.ToString(RowKeyFormatString))
+						{
+							EventName = e.Metadata.EventName,
+							AggregateId = id.Value,
+							AggregateName = e.Metadata[MetadataKeys.AggregateName],
+							AggregateSequenceNumber = e.AggregateSequenceNumber,
+							Data = e.SerializedData,
+							Metadata = e.SerializedMetadata,
+							GlobalSequenceNumber = await _uniqueIdGenerator.GetNextIdAsync().ConfigureAwait(false),
+							BatchId = Guid.Parse(e.Metadata[MetadataKeys.BatchId]),
+						}))
+				.ConfigureAwait(false);
+			var entities = entityTask.ToArray();
+			
+			_log.Verbose("Committing {0} events to Azure Storage event store for entity with ID '{1}'", serializedEvents.Count, id);
+
+			var operation = new TableBatchOperation();
+			foreach (var entity in entities)
+				operation.Add(TableOperation.Insert(entity));
+
+			var table = _factory.CreateTableReferenceForEventStore();
+			await table.ExecuteBatchAsync(operation, cancellationToken).ConfigureAwait(false);
+
+			return entities;
 		}
 
 		public async Task<IReadOnlyCollection<ICommittedDomainEvent>> LoadCommittedEventsAsync(
@@ -43,12 +116,56 @@ namespace EventFlow.AzureStorage.EventStores
 			int fromEventSequenceNumber,
 			CancellationToken cancellationToken)
 		{
-			throw new NotImplementedException();
+			var partitionKey = id.Value;
+			var partitionKeyFilter = TableQuery.GenerateFilterCondition(TableConstants.PartitionKey, QueryComparisons.Equal, partitionKey);
+
+			var rowKeyStart = fromEventSequenceNumber.ToString(RowKeyFormatString);
+			var rowKeyFilter = TableQuery.GenerateFilterCondition(TableConstants.RowKey, QueryComparisons.GreaterThanOrEqual, rowKeyStart);
+
+			var filter = TableQuery.CombineFilters(partitionKeyFilter, TableOperators.And, rowKeyFilter);
+			var query = new TableQuery<EventDataEntity>().Where(filter);
+			var table = _factory.CreateTableReferenceForEventStore();
+			var results = new List<ICommittedDomainEvent>();
+
+			TableContinuationToken token = null;
+			do
+			{
+				var resultSegment = await table.ExecuteQuerySegmentedAsync(query, token, cancellationToken).ConfigureAwait(false);
+				token = resultSegment.ContinuationToken;
+
+				results.AddRange(resultSegment.Results.Select(r => r.ToDomainEvent()));
+			} while (token != null);
+
+			return results.OrderBy(e => e.AggregateSequenceNumber).ToList().AsReadOnly();
 		}
 
 		public async Task DeleteEventsAsync(IIdentity id, CancellationToken cancellationToken)
 		{
-			throw new NotImplementedException();
+			var filter = TableQuery.GenerateFilterCondition(TableConstants.PartitionKey, QueryComparisons.Equal, id.Value);
+			var query = new TableQuery().Where(filter).Select(new[] {TableConstants.RowKey});
+			var table = _factory.CreateTableReferenceForEventStore();
+
+			TableContinuationToken token = null;
+			do
+			{
+				var resultSegment = await table.ExecuteQuerySegmentedAsync(query, token, cancellationToken).ConfigureAwait(false);
+				token = resultSegment.ContinuationToken;
+
+				var chunks = resultSegment.Results
+					.Select((x, index) => new { Index = index, Value = x })
+					.Where(x => x.Value != null)
+					.GroupBy(x => x.Index / TableConstants.TableServiceBatchMaximumOperations)
+					.Select(x => x.Select(v => v.Value));
+
+				foreach (var chunk in chunks)
+				{
+					var operation = new TableBatchOperation();
+					foreach (var entity in chunk)
+						operation.Delete(entity);
+
+					await table.ExecuteBatchAsync(operation, cancellationToken).ConfigureAwait(false);
+				}
+			} while (token != null);
 		}
 
 
