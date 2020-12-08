7c8ddc7f83e7221e472a2233997abe07653db3e6
Reyhn3 <bjorn@sentiandream.com>
2020-12-08 18:34:06 +0100

    Implement delete for ReadStore

--- Source/EventFlow.AzureStorage/ReadStores/AzureStorageReadModelStore.cs
+++ Source/EventFlow.AzureStorage/ReadStores/AzureStorageReadModelStore.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using EventFlow.Aggregates;
@@ -8,11 +9,16 @@ using EventFlow.Extensions;
 using EventFlow.Logs;
 using EventFlow.ReadStores;
 using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Azure.Cosmos.Table.Protocol;
 using Newtonsoft.Json;
 
 
 namespace EventFlow.AzureStorage.ReadStores
 {
+	/*
+	 * The read model store is using a single Azure Storage Table to store
+	 * ALL read models. The read model type is used as partition key.
+	 */
 	public class AzureStorageReadModelStore<TReadModel> : IAzureStorageReadModelStore<TReadModel>
 		where TReadModel : class, IReadModel
 	{
@@ -25,19 +31,56 @@ namespace EventFlow.AzureStorage.ReadStores
 			_factory = factory ?? throw new ArgumentNullException(nameof(factory));
 		}
 
-		public Task DeleteAsync(string id, CancellationToken cancellationToken)
-			=> throw new NotImplementedException();
+		public async Task DeleteAsync(string id, CancellationToken cancellationToken)
+		{
+			var (partitionKey, rowKey) = GetKeys(id);
+			var retrieveOperation = TableOperation.Retrieve<ReadModelEntity>(partitionKey, rowKey);
+			var table = _factory.CreateTableReferenceForReadStore();
+			var result = await table.ExecuteAsync(retrieveOperation, cancellationToken).ConfigureAwait(false);
+			if (!(result.Result is ReadModelEntity entity))
+				return;
 
-		public Task DeleteAllAsync(CancellationToken cancellationToken)
-			=> throw new NotImplementedException();
+			var deleteOperation = TableOperation.Delete(entity);
+			await table.ExecuteAsync(deleteOperation, cancellationToken).ConfigureAwait(false);
+		}
+
+		public async Task DeleteAllAsync(CancellationToken cancellationToken)
+		{
+			var partitionKey = GetPartitionKey();
+			var filter = TableQuery.GenerateFilterCondition(TableConstants.PartitionKey, QueryComparisons.Equal, partitionKey);
+			var query = new TableQuery().Where(filter).Select(new[] {TableConstants.PartitionKey, TableConstants.RowKey});
+			var table = _factory.CreateTableReferenceForReadStore();
+
+			TableContinuationToken token = null;
+			do
+			{
+				var resultSegment = await table.ExecuteQuerySegmentedAsync(query, token, cancellationToken).ConfigureAwait(false);
+				token = resultSegment.ContinuationToken;
+
+				var chunks = resultSegment.Results
+					.Select((x, index) => new { Index = index, Value = x })
+					.Where(x => x.Value != null)
+					.GroupBy(x => x.Index / TableConstants.TableServiceBatchMaximumOperations)
+					.Select(x => x.Select(v => v.Value));
+
+				foreach (var chunk in chunks)
+				{
+					var operation = new TableBatchOperation();
+					foreach (var entity in chunk)
+						operation.Delete(entity);
+
+					await table.ExecuteBatchAsync(operation, cancellationToken).ConfigureAwait(false);
+					_log.Verbose(() => $"Deleted batch of {operation.Count} Azure Storage read models '{partitionKey}'");
+				}
+			} while (token != null);
+		}
 
 		public async Task<ReadModelEnvelope<TReadModel>> GetAsync(string id, CancellationToken cancellationToken)
 		{
-			var partitionKey = typeof(TReadModel).PrettyPrint();
-			var rowKey = id;
+			var (partitionKey, rowKey) = GetKeys(id);
 			var operation = TableOperation.Retrieve<ReadModelEntity>(partitionKey, rowKey);
 			var table = _factory.CreateTableReferenceForReadStore();
-			var result = await table.ExecuteAsync(operation, CancellationToken.None).ConfigureAwait(false);
+			var result = await table.ExecuteAsync(operation, cancellationToken).ConfigureAwait(false);
 
 			if (!(result.Result is ReadModelEntity entity))
 			{
@@ -66,6 +109,11 @@ namespace EventFlow.AzureStorage.ReadStores
 			CancellationToken cancellationToken)
 			=> throw new NotImplementedException();
 
+		private static string GetPartitionKey()
+			=> typeof(TReadModel).PrettyPrint();
+
+		private static (string partitionKey, string rowKey) GetKeys(string id)
+			=> (GetPartitionKey(), id);
 
 		private class ReadModelEntity : TableEntity
 		{
